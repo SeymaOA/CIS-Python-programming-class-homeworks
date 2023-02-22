22a2abe128a78ca60e457cfdfb20c47e2f2fa79a
SeymaOA <82340301+SeymaOA@users.noreply.github.com>
2023-02-21 19:28:31 -0500

    Add files via upload

---
+++ 2.7.2 compute_change.py
@@ -0,0 +1,13 @@
+#Compute money change
+amount_to_change = int(input())
+
+num_fives = amount_to_change // 5
+
+num_ones = amount_to_change - (num_fives * 5) 
+
+print('Change for $', amount_to_change)
+print(num_fives, '$5 bill(s) and', num_ones, '$1 bill(s)')
+
+
+
+  
