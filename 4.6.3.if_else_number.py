80a7a8893700321101bd18e885b8b5146de0fa63
SeymaOA <82340301+SeymaOA@users.noreply.github.com>
2023-02-22 10:06:54 -0500

    Add files via upload

---
+++ 4.6.3.if_else_number.py
@@ -0,0 +1,7 @@
+office_num = int(input('Please enter office number: '))
+if office_num >= 100 and office_num <= 150:
+    print('Valid office number')
+elif office_num >= 200 and office_num <= 250:
+    print('Valid office number')
+else:
+    print('invalid office number')
